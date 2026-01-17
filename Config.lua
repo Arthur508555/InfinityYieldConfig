738cd6767fa9ba323c3bf6fca94697986626cda3
Arthur508555 <arthuroliveiragoncalves826@gmail.com>
2026-01-17 19:56:35 -0300

    Create Config.lua with test print statements
    Add initial configuration setup and test prints.

---
+++ Config.lua
@@ -0,0 +1,20 @@
+local Config = {}
+
+print("loading")
+print("1 %")
+print("15 %")
+print("19 %")
+print("30 %")
+print("67 %")
+print("SIX SEVEN")
+print("LOAD")
+print("TEST")
+print("TEST")
+print("TEST")
+print("TEST")
+
+task.wait(2)
+
+print("ok")
+
+  
